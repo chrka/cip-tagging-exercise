8a701fe17991d868dddae6a29928c0940f8e27f7
Christoffer Karlsson <chrka@mac.com>
2020-01-13 14:37:34 +0100

    Export symbols

--- source/README.md
+++ source/README.md
@@ -4,7 +4,7 @@
 ## Installation
 
 ```shell script
-pip install 'git+https://github.com/chrka/cip-tagging-exercise.git#egg=pkg&subdirectory=source'
+pip install 'git+https://github.com/chrka/cip-tagging-exercise.git#egg=tagger&subdirectory=source'
 ```
 
 ## Usage
---
+++ source/tagger/__init__.py
@@ -0,0 +1,8 @@
+from tagger._preprocessing.html import HTMLToText
+from tagger._preprocessing.lowercase import Lowercase
+from tagger._preprocessing.tokenization import Tokenize
+
+from tagger._featureextraction.bags import BagOfWords
+from tagger._featureextraction.tfidf import Tfidf
+
+from tagger._classification.naivebayes import NaiveBayes
