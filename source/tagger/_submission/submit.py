20a37e5cb7aad99b56d1a180b8e01324f77632e0
Christoffer Karlsson <chrka@mac.com>
2020-01-22 12:45:47 +0100

    First stab at submitting to internal leaderboard

--- source/tagger/_submission/submit.py
+++ source/tagger/_submission/submit.py
@@ -1,19 +1,44 @@
+import requests
+import pandas as pd
 from pprint import pprint
 from tagger._evaluation.metrics import hamming_loss, exact_match_ratio
 
-def submit_model(model, *, team_name, model_name,
-                 local_events=None, local_tags=None):
-    if local_events is None:
-        raise NotImplementedError("NYI")
+
+def submit_model(model, events, *, team_name, model_name,
+                 local_tags=None,
+                 base_url=None):
+    print(f"Team '{team_name}' submitting model '{model_name}':")
+    pprint(model)
+    print(72 * '-')
+
+    tags_pred = model.predict(events)
+
+    if local_tags is None:
+        assert base_url is not None
+        url = base_url + "submit"
+        files = {
+            'submission.csv': pd.DataFrame(tags_pred).to_csv(index=False)
+        }
+        data = {
+            'team': 'All about the base',
+            'model_description': 'Baseline'
+        }
+
+        r = requests.post(url, data=data, files=files)
+
+        try:
+            r.raise_for_status()
+            result = r.json()
+            loss = result['hamming_loss']
+            emr = result['exact_match_ratio']
+        # TODO: Be more specific
+        except Exception:
+            print("Submission failed!")
+            return
     else:
-        print(f"Team '{team_name}' submitting model '{model_name}':")
-        pprint(model)
-        print(72 * '-')
-
-        tags_pred = model.predict(local_events)
-
         loss = hamming_loss(local_tags, tags_pred)
-        print(f"Hamming loss for submission: {loss}")
-
         emr = exact_match_ratio(local_tags, tags_pred)
-        print(f"Exact match ratio for submission: {emr}")
+
+    # TODO: Remove
+    print(f"Hamming loss for submission: {loss}")
+    print(f"Exact match ratio for submission: {emr}")
